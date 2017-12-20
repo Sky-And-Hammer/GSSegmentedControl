5b45a0a39c206e50d213160e6be84f4a3c69140d
GloomySunday <121055230@qq.com>
2017-12-20 20:08:01 +0800

    Add: gssegmentedcontrol initialized

---
+++ GSSegmentedControl/GSSegmentedControl.h
@@ -0,0 +1,19 @@
+//
+//  GSSegmentedControl.h
+//  GSSegmentedControl
+//
+//  Created by 孟钰丰 on 2017/12/20.
+//  Copyright © 2017年 孟钰丰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GSSegmentedControl.
+FOUNDATION_EXPORT double GSSegmentedControlVersionNumber;
+
+//! Project version string for GSSegmentedControl.
+FOUNDATION_EXPORT const unsigned char GSSegmentedControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GSSegmentedControl/PublicHeader.h>
+
+
